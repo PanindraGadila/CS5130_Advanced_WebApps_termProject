6af749aab35ad37e422be09139af73d4104117f4
PanindraGadila <112538866+PanindraGadila@users.noreply.github.com>
2022-12-13 17:38:16 -0600

    First add of all project files
    Added all project files from local after the school gradings

---
+++ digi_bookhoard/includes/footer.php
@@ -0,0 +1,10 @@
+<section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; Digi-Bookhoard (University Library management system) | Designed by : Panindra & Srikanth</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
